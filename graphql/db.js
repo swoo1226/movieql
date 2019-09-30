4677da10c787d8766987b607d9716cf45e1644a2
swoo1226 <copycat1226@gmail.com>
2019-09-30 15:54:38 +0900

    basic movies data setting

--- graphql/db.js
+++ graphql/db.js
@@ -1,25 +1,35 @@
-export const people = [
-    {
-        id: 0,
-        name: 'swoo',
-        age: 29,
-        gender: 'male'
-    },
-    {
-        id: 1,
-        name: 'areum',
-        age: 30,
-        gender: 'female'
-    },
-    {
-        id: 2,
-        name: 'coooper',
-        age: 26,
-        gender: 'male'
-    }
-]
+export let movies = [
+  {
+    id: 0,
+    name: "해바라기",
+    score: "4"
+  },
+  {
+    id: 1,
+    name: "바람",
+    score: "5"
+  },
+  {
+    id: 2,
+    name: "타짜",
+    score: "4"
+  }
+];
+
+export const getMovies = () => movies;
 
 export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === id);
-    return filteredPeople[0];
-}
+  const filteredMovies = movies.filter(movie => movie.id === id);
+  return filteredMovies[0];
+};
+
+// 해당하는 id의 영화만 목록에서 지운다.
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id !== id);
+  if (movies.lennght > cleanedMovies.length) {
+    movies = cleandMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
