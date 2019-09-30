a5eda3d233455a3277e31c8257e8c8eb622710b1
swoo1226 <copycat1226@gmail.com>
2019-09-30 17:50:36 +0900

    YTS API 세팅중

--- graphql/db.js
+++ graphql/db.js
@@ -1,58 +1,6 @@
-export let movies = [
-  {
-    id: 0,
-    name: "해바라기",
-    score: "4"
-  },
-  {
-    id: 1,
-    name: "바람",
-    score: "5"
-  },
-  {
-    id: 2,
-    name: "타짜",
-    score: "4"
-  }
-];
+import fetch from 'node-fetch'
+const API_URL = 'https://yts.lt/api/v2/list_movies.json';
 
-export const getMovies = () => movies;
-
-export const getById = id => {
-  const filteredMovies = movies.filter(movie => movie.id === id);
-  return filteredMovies[0];
-};
-
-// 해당하는 id의 영화만 목록에서 지운다.
-export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id !== id);
-  if (movies.lennght > cleanedMovies.length) {
-    movies = cleandMovies;
-    return true;
-  } else {
-    return false;
-  }
-};
-
-// 영화를 추가하는 함수
-export const addMovie = (name, score) => {
-  let hasSameMovie = () => {
-    for (let i = 0; i < movies.length; i++) {
-      if (movies[i].name === name) {
-        return false;
-      }
-    }
-    return true;
-  };
-  if (hasSameMovie()) {
-    const newMovie = {
-      id: `${movies.length} + 1`,
-      name,
-      score
-    };
-    movies.push(newMovie);
-    return newMovie;
-  } else {
-    return false;
-  }
-};
+const getMovies = (limit, rating) => {
+    fetch(${API_URL}).then(res => res.json).then(json => json.data.movies)
+}
